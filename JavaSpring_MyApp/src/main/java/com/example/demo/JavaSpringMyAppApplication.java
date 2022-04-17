336dae2b1c70b216221836a54f91b065eb2e2097
saku-2 <yukiko.tsuda.1026@gmail.com>
2022-04-17 20:13:43 +0900

    テストコミット

---
+++ JavaSpring_MyApp/src/main/java/com/example/demo/JavaSpringMyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMyAppApplication.class, args);
+	}
+
+}
--- JavaSpring_MyApp/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module JavaSpring_MyApp {
-}
