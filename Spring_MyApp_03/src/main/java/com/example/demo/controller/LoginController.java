20dabc777c191e06fda9b1f73e374413b6edcd75
saku-2 <yukiko.tsuda.1026@gmail.com>
2022-04-23 12:44:26 +0900

    .ymlほか

--- Spring_MyApp_03/src/main/java/com/example/demo/controller/LoginController.java
+++ Spring_MyApp_03/src/main/java/com/example/demo/controller/LoginController.java
@@ -91,12 +91,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping(value="/login")
 public class LoginController {
 
-	@RequestMapping(value="/login")
+	@GetMapping(value="/login")
 	private String hello(LoginForm loginForm, 
 			Model model,
 			@ModelAttribute("complete") String complete){
